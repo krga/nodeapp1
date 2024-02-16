084648d454306de2e845b0ef7cfb2bf31b40c289
Kristian Gavran <krga@users.noreply.github.com>
2024-02-16 23:31:32 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+const env = process.env.NODE_ENV;
+
+app.get('/', (req, res) => {
+  res.send('Hello '+env+': Version 01!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
